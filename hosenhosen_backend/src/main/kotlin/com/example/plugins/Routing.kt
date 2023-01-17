55a13c57045b843f85cf8455e0eaeb5ed5ba7572
Rostin11 <robinstalder1@hotmail.com>
2023-01-17 10:49:39 +0100

    Setup Connection to Database in Backend

--- hosenhosen_backend/src/main/kotlin/com/example/plugins/Routing.kt
+++ hosenhosen_backend/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,32 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.DriverManager
+import java.sql.DriverManager.getConnection
+import java.sql.PreparedStatement
+
 
 fun Application.configureRouting() {
 
     routing {
         get("/products") {
             call.respond("")
+            createDBConnection()
         }
     }
 }
+
+fun createDBConnection(){
+    val myConn = getConnection("jdbc:mariadb://localhost:3306/", "root", "1234")
+    Class.forName ("org.mariadb.jdbc.Driver");
+
+    val prepareStatement = myConn.prepareStatement("")
+
+// Submit the query
+    var executeQuery = prepareStatement.executeQuery()
+}
+
